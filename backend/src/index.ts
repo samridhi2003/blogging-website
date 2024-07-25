86917c80e06a88e029d0112a4b2f7b9dd1b414aa
Sanjay Kumar Singh <sanjaykumarsingh@Samridhis-Macbook.local>
2024-07-25 16:17:14 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono'
+import { userRouter } from './routes/user'
+import {blogRouter} from './routes/blog';
+ 
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+    JWT_SECRET: string
+  }
+}>()
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
+
+// app.use('/api/v1/blog/*', async (c, next)=>{
+//   const header= c.req.header("Authorization");
+//   if(!header){
+//     c.status(401);
+//     return c.json({error: "unauthorized"});
+//     }
+//     const token= header.split(' ')[1];
+//     const payload= await verify(token, c.env.JWT_SECRET);
+//     if(!payload){
+//       c.status(401);
+//       return c.json({error:"unauthorized"});
+//     }
+//     c.set( payload.id);
+//     await next()
+//   })
+
+
+
+export default app
